1816fddb002b961dfa909349bc49e00f30d1e63e
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2021-05-03 16:19:55 +0300

    Implemented Dijkstra algorithm
    Dijkstra finds the shortest route, which is not what we want. We want
    the route with the greatest score. That should be the route with the
    maximum used trains and covering the maximum extra tickets.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum City {
     Edinburgh,
     London,
@@ -457,6 +457,52 @@ fn get_tickets() -> Vec<Ticket> {
     tickets
 }
 
+fn dijkstra(source: City) -> (HashMap<City, u8>, HashMap<City, City>) {
+    let graph = create_network();
+    let mut dist: HashMap<City, u8> = HashMap::new();
+    let mut prev: HashMap<City, City> = HashMap::new();
+    let mut q: Vec<City> = Vec::new();
+    let mut u: City = City::Dieppe;
+
+    // initialize distances to 100 and set the distance of source to 0.
+    // Since there are only 45 trains available in the game, the value of
+    // 100 is basically infinity.
+    for (city, _) in graph.iter() {
+        dist.insert(city.clone(), 100);
+        q.push(city.clone());
+    }
+    dist.insert(source, 0);
+
+    // The main loop
+    while !q.is_empty() {
+        u = q[0];
+        for city in q.iter() {
+            if dist.get(&city).unwrap() < dist.get(&u).unwrap() {
+                u = city.clone();
+            }
+        }
+        // Remove this vertex from q
+        let index = q.iter().position(|&x| x == u).unwrap();
+        q.remove(index);
+
+        // Check the neighbours of u for corrections
+        let destinations = graph.get(&u).unwrap();
+        for (destination, distance) in destinations.iter() {
+            if q.iter().any(|i| i == destination) {
+                let alt = dist.get(&u).unwrap() + *distance;
+                // Maximum number of trains in game is 45, so you can't have a distance
+                // greater than that.
+                if alt < *dist.get(&destination).unwrap() && alt <= 45 {
+                    dist.insert(destination.clone(), alt);
+                    prev.insert(destination.clone(), u.clone());
+                }
+            }
+        }
+        // Get the vertex with the smallest distance from q.
+    }
+    return (dist, prev);
+}
+
 fn main() {
     let routes = create_network();
     let big_tickets = get_big_tickets();
@@ -482,6 +528,18 @@ fn main() {
         for (destination, distance) in destinations {
             println!("--> {:?} with {} trains", destination, distance);
         }
+    }
+
+    // Test dijkstra
+    let (dist, prev) = dijkstra(City::Edinburgh);
+    println!(
+        "Distance Edinburgh to Athens is {}",
+        dist.get(&City::Athina).unwrap()
+    );
+    let mut current = City::Athina;
+    while current != City::Edinburgh {
+        println!("Distance to {:?} is {}", current, dist.get(&current).unwrap());
+        current = *prev.get(&current).unwrap();
     }
 }
 
