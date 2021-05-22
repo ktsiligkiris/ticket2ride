a4f8e30827f452c868c9ab882ad16e34b01ca6a2
Konstantinos Tsiligkiris <ktsiligkiris@outlook.com>
2021-05-22 23:00:59 +0300

    Refactor scoring
    I moved the logic outside the public function and into separate
    private functions that handle separately each case.

--- src/scoring.rs
+++ src/scoring.rs
@@ -2,20 +2,40 @@ use ticket2ride::{create_network, get_big_tickets, get_tickets, City};
 
 pub fn get_scores(route: Vec<City>) -> u16 {
     let mut score: u16 = 0;
-    let routes = create_network();
+
+    score += big_ticket_score(&route);
+    score += ticket_score(&route);
+    score += trains_score(&route);
+    score
+}
+
+fn big_ticket_score(route: &Vec<City>) -> u16 {
+    let mut score: u16 = 0;
     let big_tickets = get_big_tickets();
-    let small_tickets = get_tickets();
     for ticket in big_tickets.iter() {
         if route.iter().any(|&i| i == ticket.depart) && route.iter().any(|&i| i == ticket.arrive) {
             score += ticket.value as u16;
         }
     }
+    score
+}
+
+fn ticket_score(route: &Vec<City>) -> u16 {
+    let mut score: u16 = 0;
+    let small_tickets = get_tickets();
+
     for ticket in small_tickets.iter() {
         if route.iter().any(|&i| i == ticket.depart) && route.iter().any(|&i| i == ticket.arrive) {
             score += ticket.value as u16;
         }
     }
-    // Get score from used trains
+    score
+}
+
+fn trains_score(route: &Vec<City>) -> u16 {
+    let mut score: u16 = 0;
+    let routes = create_network();
+
     for (depart, arrive) in route
         .split_last()
         .unwrap()
