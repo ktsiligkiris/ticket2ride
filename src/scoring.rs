1846ee4b4ea7e3f41516d18871eea3ae2ceaacba
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2021-05-22 17:45:37 +0300

    Augment get_scores
    This way I can add to the score the trains used in the route!

--- src/scoring.rs
+++ src/scoring.rs
@@ -1,7 +1,8 @@
-use ticket2ride::{get_big_tickets, get_tickets, City};
+use ticket2ride::{create_network, get_big_tickets, get_tickets, City};
 
 pub fn get_scores(route: Vec<City>) -> u8 {
     let mut score: u8 = 0;
+    let routes = create_network();
     let big_tickets = get_big_tickets();
     let small_tickets = get_tickets();
     for ticket in big_tickets.iter() {
@@ -13,6 +14,16 @@ pub fn get_scores(route: Vec<City>) -> u8 {
         if route.iter().any(|&i| i == ticket.depart) && route.iter().any(|&i| i == ticket.arrive) {
             score += ticket.value;
         }
+    }
+    // Get score from used trains
+    for (depart, arrive) in route
+        .split_last()
+        .unwrap()
+        .1
+        .iter()
+        .zip(route.split_first().unwrap().1.iter())
+    {
+        score += routes[&depart][&arrive] as u16;
     }
     score
 }
